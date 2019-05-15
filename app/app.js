da4a4199b9447feed44b43ff03159fc7c9ee48bd
Sonia <sonia@gmail.com>
2019-05-15 14:50:17 +0100

    using async

--- app/app.js
+++ app/app.js
@@ -18,86 +18,83 @@
 const fs = require('fs');
 const readline = require('readline');
 const {google} = require('googleapis');
+const util = require('util');
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/gmail.send'];
+const SCOPES = ['https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/gmail.send'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
 //const TOKEN_PATH = '/secrets/token.json';
+const CREDENTIALS_PATH = 'c:\\secrets\\credentials.json';
 const TOKEN_PATH = 'c:\\secrets\\token.json';
 
-// Load client secrets from a local file.
-//fs.readFile('/secrets/credentials.json', (err, content) => {
-fs.readFile('c:\\secrets\\credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Drive API.
-  authorize(JSON.parse(content), sendEmail);
-});
 
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
- */
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) {
-    //  throw new Error(err);
-    return getAccessToken(oAuth2Client, callback);
-    }
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
+(async function(){
+
+  var credentials= await getCredentials()
+  const oAuth2Client = new google.auth.OAuth2(credentials.installed.client_id, credentials.installed.client_secret, credentials.installed.redirect_uris[0]);  
+  var token = await getToken()
+  //var newToken = await getAccessToken(oAuth2Client)
+  //console.log(JSON.stringify(newToken))
+  //return;
+  oAuth2Client.setCredentials(token);
+
+
+  var uploadedFileInfo = await uploadFileAsync(oAuth2Client,'c:\\secrets\\app.apk')
+  console.log(uploadedFileInfo)
+  return;
+  var emailParams={
+    fromName:'Sonia',
+    fromAddress:'sonia@gmail.com',
+    to :"mig.ruiz@gmail.com",
+    subject: "subject",
+    body: "body"
+  }
+  var result =  await sendEmailv2(oAuth2Client,emailParams)
+  console.log(result)
+})();
+return;
+
+
+async function getCredentials(){
+  var content =  await util.promisify(fs.readFile)(CREDENTIALS_PATH)
+  return JSON.parse(content)
 }
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getAccessToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
+async function getToken(){
+  var content =  await util.promisify(fs.readFile)(TOKEN_PATH)
+  return JSON.parse(content)
 }
 
 
 
-function process(){
-  
-}
-
-
-
-function sendEmail(auth,emailParams) {
+function getAccessToken(oAuth2Client) {
   return new Promise(function(resolve, reject) {
+    const authUrl = oAuth2Client.generateAuthUrl({
+      access_type: 'offline',
+      scope: SCOPES,
+    });
+    console.log('Authorize this app by visiting this url:', authUrl);
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    rl.question('Enter the code from that page here: ', (code) => {
+      rl.close();
+      oAuth2Client.getToken(code, (err, token) => {
+        if (err) {
+          reject(err)
+        }
+        else{
+          resolve(token)
+        }
+      });
+    });
+  })
+}
+
+async function sendEmailv2(auth,emailParams) {
     var gmailClass = google.gmail('v1');
 
     var email_lines = [];
@@ -114,27 +111,20 @@ function sendEmail(auth,emailParams) {
 
     var base64EncodedEmail = new Buffer(email).toString('base64');
     base64EncodedEmail = base64EncodedEmail.replace(/\+/g, '-').replace(/\//g, '_');
-
-    gmailClass.users.messages.send({
+    return await gmailClass.users.messages.send({
       auth: auth,
       userId: 'me',
       resource: {
         raw: base64EncodedEmail
       }
-    }, function (err, results) {
-      if (err) {
-        reject(err)
-      } else {
-        console.log(results)
-        resolve(results)
-        }
-      });
-  });
+    });
 }
 
 
-function uploadFile(auth,fileLocation,onSharedLink){
-  return new Promise(function(resolve, reject) {
+
+
+
+async function uploadFileAsync(auth,fileLocation){
     const drive = google.drive({version: 'v3', auth});
     var fileMetadata = {
       'name': 'app.apk'
@@ -143,19 +133,11 @@ function uploadFile(auth,fileLocation,onSharedLink){
       mimeType: 'application/vnd.android.package-archive',
       body: fs.createReadStream(fileLocation)
     };
-    drive.files.create({
+    return await drive.files.create({
       resource: fileMetadata,
       media: media,
       fields: 'id'
-    }, function (err, file) {
-      if (err) {
-        reject(err)
-      } 
-      else {   
-        resolve(file)   
-        }
-      });
-  });
+    });
   }
 
   
